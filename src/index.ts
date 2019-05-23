85547456f6b6cfb0576acb6bee843e7eb654c5e6
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-05-23 17:33:48 -0400

    add user register login and synth get all routes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
+import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import morgan from "morgan";
 import path from "path";
 import connect from "./db/init";
-dotenv.config();
+import apiRoutes from "./routes/api";
 const app = express();
 const port = process.env.SERVER_PORT; // default port to listen
 const db = process.env.MONGODB_DEV;
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
 // init config
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-// define a route handler for the default home page
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.use("/api/", apiRoutes);
+// define a route handler for the default home page
 
 // start the Express server
 app.listen(port, () => {
