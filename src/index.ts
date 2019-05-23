fbc4bfccec52371db4c836be97301b062d780000
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-05-23 11:12:55 -0400

    configure ejs template

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 const app = express();
 const port = 8080; // default port to listen
 
+app.settings("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 // start the Express server
